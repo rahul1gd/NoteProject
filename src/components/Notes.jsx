0e483c6854fd824a91537ca6be1691f80b837649
Rahul Kumar <91670525+rahul1gd@users.noreply.github.com>
2022-09-03 19:17:08 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div class="note">
+      <h1>This is React Bootcamp</h1>
+      <p>
+        Inbound marketing is an advertising philosophy catered to the digital
+        age. Inbound marketing is based on the idea that your ideal audience is
+        already out there; as a marketer, your job is to attract them and
+        convince them to use your product or service.
+      </p>
+    </div>
+  );
+}
+
+export default Notes;
